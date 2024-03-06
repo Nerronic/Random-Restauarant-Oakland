078d9466e1a7c2ce704c0214b5d79abe071e18d0
Ty <tyrellcarlton@gmail.com>
2024-03-06 12:04:38 -0800

    first commit

--- src/components/oakland-restaurants.jsx
+++ src/components/oakland-restaurants.jsx
@@ -1,27 +1,31 @@
-import React,{useState} from 'react'
-import {oakData} from './oak-data';
+import React, { useState } from "react";
+import { oakData } from "./oak-data";
 
 function OakRestaurants() {
-const [selectedRestaurant,SetSelectedRestaurant] = useState(0);
+  const [selectedRestaurant, SetSelectedRestaurant] = useState(0);
 
-function handleClick() {
-SetSelectedRestaurant(selectedRestaurant + 1);
-}
+  function handleClick() {
+    SetSelectedRestaurant(selectedRestaurant + 1);
+  }
 
-let selection = oakData[selectedRestaurant];
+  let selection = oakData[selectedRestaurant];
 
   return (
-    <div><h1 className='header'> Top 50 Restauarants in Oakland</h1>
-    <div className='restaurant-container'>
+    <div>
+      <h1 className="header"> Top 50 Restauarants in Oakland</h1>
+      <div className="restaurant-container">
         <h2> {selection.name}</h2>
         <h3> {selection.location}</h3>
-<h3> {selection.yelpRating}</h3>
-<h3> {selection.website}</h3>
+        <h3> {selection.yelpRating}</h3>
+        <h3> {selection.website}</h3>
 
-<button id='generate-button' onClick={handleClick}> Let's Eat</button>
+        <button id="generate-button" onClick={handleClick}>
+          {" "}
+          Let's Eat
+        </button>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default OakRestaurants
+export default OakRestaurants;
