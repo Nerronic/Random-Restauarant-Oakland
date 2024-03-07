b181e92698ba54e5a869607bbfefe28968072965
Ty <tyrellcarlton@gmail.com>
2024-03-06 17:11:04 -0800

    updated the comment box

--- src/components/oakland-restaurants.jsx
+++ src/components/oakland-restaurants.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { oakData } from "./oak-data";
-
+import Comments from "./comments"
 function OakRestaurants() {
   const [selectedRestaurant, SetSelectedRestaurant] = useState(0);
 
@@ -17,12 +17,19 @@ function OakRestaurants() {
         <h2> {selection.name}</h2>
         <h3> {selection.location}</h3>
         <h3> {selection.yelpRating}</h3>
-        <h3> {selection.website}</h3>
+        <h3 > {selection.website}</h3>
 
         <button id="generate-button" onClick={handleClick}>
-          {" "}
+          
           Let's Eat
         </button>
+
+        <h3> Like what you see? Let us know! <br/> Drop a comment below</h3>
+        
+       <Comments/>
+
+        
+        
       </div>
     </div>
   );
